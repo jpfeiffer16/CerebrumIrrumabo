03db7295112a2d817f372b78c081e725da82576a
jpfeiffer16 <jpfeiffer@bluemodus.com>
2015-07-03 00:05:06 -0600

    Added basic debug features

--- client/CerebrumIrrumabo.js
+++ client/CerebrumIrrumabo.js
@@ -5,91 +5,146 @@ var version = 0.1;
 var TIMER = false;
 
 var CerebrumIrrumabo = function(input, code) {
-    
-    this.pointerLength = 30;
-    
     var that = this;
     
     
-	this.run = function() {
-        return interpret(input, code, that.pointerLength);
-        setTimeout(function() {
-            TIMER = true;
-        }, 50000);
+    var debug = false;
+    var pointerLength = 30;
+    var debugValues = {
+        pointer: [],
+        pointerPos: 0,
+        inputPos: 0,
+        codePos: 0,
+        codeChar: '',
     };
-};
-
-
-
-
-
-function interpret(input, code, pointerLength) {
-	var pointerPos = 0,
+    
+    
+    var pointerPos = 0,
+    		inputPointer = 0,
+    		inputPointerIn = 0,
+    		pointer = initializePointer(pointerLength),
+    		outputString = '';
+    function step() {
+    	// var input = this.input, code = this.code;
+        if (inputPointer <= code.length - 1) {
+            var ch = code.charAt(inputPointer);
+            if (ch == "+") {
+                pointer[pointerPos] = pointer[pointerPos] + 1;
+            }
+            if (ch == "-") {
+                pointer[pointerPos] = pointer[pointerPos] - 1;
+                if (pointer[pointerPos] < 0) {
+                    pointer[pointerPos] = 0;
+                }
+            }
+            if ( ch == "<") {
+                pointerPos--;
+                if (pointerPos < 0) {
+                    // pointerPos = 0;
+                    throw 'Out of bounds';
+                }
+            }
+            if (ch == ">") {
+                pointerPos++;
+                if (pointerPos > pointerLength) {
+                    throw 'Out of bounds';
+                }
+            }
+            if (ch == ",") {
+    			var r; //TODO fix this
+                if (inputPointerIn < input.length) {
+                    r = input.charCodeAt(inputPointerIn);
+                    pointer[pointerPos] = r;
+                } else {
+                    pointer[pointerPos] = 0;
+                }
+                inputPointerIn = inputPointerIn + 1;
+            }
+            if (ch == ".") {
+                outputString = outputString + String.fromCharCode(pointer[pointerPos]);
+            }
+            if (ch == "[") {
+                if (pointer[pointerPos] == 0) {
+                    var a = "";
+                    while (a != "]") {
+                        inputPointer = inputPointer + 1;
+                        a = code.charAt(inputPointer);
+                    }
+                }
+            }
+            if (ch == "]") {
+                if(pointer[pointerPos] != 0) {
+                    var a = "";
+                    while(a != "[") {
+                        inputPointer = inputPointer - 1;
+                        a = code.charAt(inputPointer);
+                    }
+                }
+            }
+            inputPointer++;
+            if (this.debug) {
+                return {
+                    pointer: pointer,
+                    pointerPos: pointerPos,
+                    inputPos: inputPointerIn,
+                    codePos: inputPointer - 1,
+                    output: outputString
+                };
+            } else {
+                return false;
+            }
+        } else {
+            return true;
+        }
+        
+        
+    	// return outputString;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+	var run = function() {
+        if (!this.debug) {
+            pointerPos = 0,
+    		inputPointer = 0,
+    		inputPointerIn = 0,
+    		pointer = initializePointer(pointerLength),
+    		outputString = '';
+            var endOfCode = false;
+            while (!endOfCode) {
+                endOfCode = step();
+            }
+            return outputString;
+        } else {
+            return null;
+        }
+    };
+    
+    var reset = function () {
+        pointerPos = 0,
 		inputPointer = 0,
 		inputPointerIn = 0,
 		pointer = initializePointer(pointerLength),
 		outputString = '';
-	
-	
-    while(inputPointer <= code.length - 1 && !TIMER) {
-        var ch = code.charAt(inputPointer);
-        if (ch == "+") {
-            pointer[pointerPos] = pointer[pointerPos] + 1;
-        }
-        if (ch == "-") {
-            pointer[pointerPos] = pointer[pointerPos] - 1;
-            if (pointer[pointerPos] < 0) {
-                pointer[pointerPos] = 0;
-            }
-        }
-        if ( ch == "<") {
-            pointerPos--;
-            if (pointerPos < 0) {
-                // pointerPos = 0;
-                throw 'Out of bounds';
-            }
-        }
-        if (ch == ">") {
-            pointerPos++;
-            if (pointerPos > pointerLength) {
-                throw 'Out of bounds';
-            }
-        }
-        if (ch == ",") {
-			var r; //TODO fix this
-            if (inputPointerIn < input.length) {
-                r = input.charCodeAt(inputPointerIn);
-                pointer[pointerPos] = r;
-            } else {
-                pointer[pointerPos] = 0;
-            }
-            inputPointerIn = inputPointerIn + 1;
-        }
-        if (ch == ".") {
-            outputString = outputString + String.fromCharCode(pointer[pointerPos]);
-        }
-        if (ch == "[") {
-            if (pointer[pointerPos] == 0) {
-                var a = "";
-                while (a != "]") {
-                    inputPointer = inputPointer + 1;
-                    a = code.charAt(inputPointer);
-                }
-            }
-        }
-        if (ch == "]") {
-            if(pointer[pointerPos] != 0) {
-                var a = "";
-                while(a != "[") {
-                    inputPointer = inputPointer - 1;
-                    a = code.charAt(inputPointer);
-                }
-            }
-        }
-        inputPointer++;
-    }
-	return outputString;
-}
+    };
+    
+    return {
+        input: input,
+        code: code,
+        debug: debug,
+        pointerLength: pointerLength,
+        debugValues: debugValues,
+        run: run,
+        step: step,
+        reset: reset
+    };
+};
+
 
 function initializePointer(plen) {
 	var pointerArray = new Array(plen);
@@ -98,3 +153,7 @@ function initializePointer(plen) {
     }
 	return pointerArray;
 }
+
+
+
+
